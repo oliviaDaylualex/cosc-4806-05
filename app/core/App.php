cb47b2a80bec0dc9a013c1aef8f1df6c644e3c0e
oliviaDaylualex <oliviaalex799@gmail.com>
2025-07-15 17:45:44 +0000

    updated codes of core

--- app/core/App.php
+++ app/core/App.php
@@ -5,19 +5,19 @@ class App {
     protected $params     = [];
 
     public function __construct() {
-       
+
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
         }
 
-        
+
         $uri      = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
         $trimmed  = trim($uri, '/');
         $segments = $trimmed === ''
             ? []
             : array_values(array_filter(explode('/', $trimmed), 'strlen'));
 
-      
+
         if (isset($segments[0]) && $segments[0] !== '') {
             $cand = ucfirst(strtolower($segments[0]));
             $file = __DIR__ . '/../controllers/' . $cand . '.php';
@@ -27,20 +27,20 @@ class App {
             }
         }
 
-        
+
         require_once __DIR__ . '/../controllers/' . $this->controller . '.php';
         $this->controller = new $this->controller;
 
-        
+
         if (isset($segments[0]) && method_exists($this->controller, $segments[0])) {
             $this->method = $segments[0];
             array_shift($segments);
         }
 
-       
+
         $this->params = $segments;
 
-  
+
         call_user_func_array(
             [ $this->controller, $this->method ],
             $this->params
