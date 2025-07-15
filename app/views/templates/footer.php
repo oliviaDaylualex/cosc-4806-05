8f90f623df4c3471c1e410989e074490fe5b496f
oliviaDaylualex <oliviaalex799@gmail.com>
2025-07-15 17:38:05 +0000

    updated footer

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,13 +1,35 @@
-<footer class="footer">    
-    <div class="row">
-        <div class="col-lg-12">
-            <p>Copyright &copy; <?php echo date('Y'); ?> </p>
-        </div>
     </div>
-</footer>
+    <footer class="footer mt-auto py-4 bg-dark text-white">
+        <div class="container">
+            <div class="row text-center text-md-start">
 
-</div>
+                <div class="col-md-4 mb-3 mb-md-0">
+                    <h5 class="mb-1">PromptMe</h5>
+                    <small>&copy; <?php echo date('Y'); ?> Remindly - All rights reserved.</small>
+                </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+                <div class="col-md-4 mb-3 mb-md-0">
+                    <h6 class="mb-2">Links</h6>
+                    <a href="https://algomau.ca" class="text-white d-block mb-1">Contact</a>
+                    <a href="https://algomau.ca" class="text-white d-block mb-1">Privacy Policy</a>
+                    <a href="https://algomau.ca" class="text-white d-block">Terms of Service</a>
+                </div>
+
+                <div class="col-md-4">
+                    <h6 class="mb-2">Follow Us</h6>
+                    <a href="https://twitter.com/" target="_blank" class="text-white me-3">
+                        <i class="bi bi-twitter"></i> Twitter
+                    </a>
+                    <a href="https://github.com/" target="_blank" class="text-white">
+                        <i class="bi bi-github"></i> GitHub
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 </body>
 </html>
